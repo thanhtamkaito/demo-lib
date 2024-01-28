ca7488ed3a797ce3507505f1fb142484ee97b01b
kaito\thanhtam <vutam1412@gmail.com>
2024-01-28 17:30:16 +0700

    Init

--- src/test/java/com/example/demolib/DemoLibApplicationTests.java
+++ src/test/java/com/example/demolib/DemoLibApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.demolib;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
-@SpringBootTest
+@TestConfiguration
 class DemoLibApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+
+    @Test
+    void testAdd() {
+        assert SimpleMath.add(1, 1) == 2;
+    }
 }
