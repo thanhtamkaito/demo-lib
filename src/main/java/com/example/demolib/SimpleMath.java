4f85d5c9074076d105861877042698ac3b05eb0e
kaito\thanhtam <vutam1412@gmail.com>
2024-01-28 17:28:53 +0700

    Init

---
+++ src/main/java/com/example/demolib/SimpleMath.java
@@ -0,0 +1,25 @@
+package com.example.demolib;
+
+
+
+public class SimpleMath {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+}
