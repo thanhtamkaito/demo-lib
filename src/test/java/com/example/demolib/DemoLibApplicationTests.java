a731a8f73a1b212ed9c5959a70f5bf50e68638c9
kaito\thanhtam <vutam1412@gmail.com>
2024-01-28 17:28:36 +0700

    first commit

---
+++ src/test/java/com/example/demolib/DemoLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
